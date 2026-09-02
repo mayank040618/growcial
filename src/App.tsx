f7c4cb8981959e2627404f2e9a6c33bb8c772aa0
Mayank Raj <mayankrajjha77@gmail.com>
2026-09-02 23:37:35 +0530

    Revert "Add classy luxury preloader animation on site load"
    This reverts commit 0cc6345e4756b5ec28147fb3ec0c73c776fd1934.

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,10 @@ import Navbar from './components/sections/Navbar';
 import Hero from './components/sections/Hero';
 import Services from './components/sections/Services';
 import Footer from './components/sections/Footer';
-import Preloader from './components/ui/Preloader';
 
 function App() {
   return (
     <div className="app-container">
-      <Preloader />
       <Navbar />
       <main>
         <Hero />
