0cc6345e4756b5ec28147fb3ec0c73c776fd1934
Mayank Raj <mayankrajjha77@gmail.com>
2026-09-02 23:36:05 +0530

    Add classy luxury preloader animation on site load

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import Navbar from './components/sections/Navbar';
 import Hero from './components/sections/Hero';
 import Services from './components/sections/Services';
 import Footer from './components/sections/Footer';
+import Preloader from './components/ui/Preloader';
 
 function App() {
   return (
     <div className="app-container">
+      <Preloader />
       <Navbar />
       <main>
         <Hero />
